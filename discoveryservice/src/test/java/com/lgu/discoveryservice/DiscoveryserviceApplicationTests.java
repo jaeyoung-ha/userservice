c98224cb790dbace80dbf48935190606918ab4a7
Jaeyoung Ha <yangaa47@gmail.com>
2021-12-02 15:39:14 +0900

    add discovery service

---
+++ discoveryservice/src/test/java/com/lgu/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgu.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
